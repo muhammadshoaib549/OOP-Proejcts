9cd9bf86af634ce46bc4501647801c954d5ad6f9
Mian Shoaib <muhammadshoaibshahid4@gmail.com>
2026-03-19 00:29:19 +0500

    Delete Bank Management System(OOP)/3rd_Semester_oops_project directory

--- Bank Management System(OOP)/3rd_Semester_oops_project/Projectcode/InterestCalculator.java
+++
@@ -1,8 +0,0 @@
-public class InterestCalculator {
-
-    public static double calculateInterest(SavingAccount savingsAccount) {
-        double balance = savingsAccount.getBalance();
-        double interestRate = savingsAccount.getInterestRate();
-        return balance * (interestRate / 100);
-    }
-}
